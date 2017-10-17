55100b91c81a9d45feaae76960ba8108e55deca8
Andres Gajardo <andresegajardo@gmail.com>
2017-10-17 11:40:34 -0400

    Installed express-session, require middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
 const express = require('express');
-const passport = require('passport');
+const expressSession = require('express-session');
+// const passport = require('passport');
+const passport = require('./middlewares/authentication');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('./config/keys');
@@ -11,27 +12,30 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-  }, 
-  (accessToken, refreshToken, profile, done) =>{
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
-  })
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
- 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Enable sessions & passport
+app.use(expressSession(({ secret: 'keyboard cat', resave: false, saveUninitialized: true })));
+
+// passport.use(
+//   new GoogleStrategy({
+//     clientID: keys.googleClientID,
+//     clientSecret: keys.googleClientSecret,
+//     callbackURL: '/auth/google/callback'
+//   }, 
+//   (accessToken, refreshToken, profile, done) =>{
+//     console.log('access token', accessToken);
+//     console.log('refresh token', refreshToken);
+//     console.log('profile', profile);
+//   })
+// );
+
+// app.get('/auth/google', passport.authenticate('google', {
+//   scope: ['profile', 'email']
+// }));
+
+// app.get('/auth/google/callback', passport.authenticate('google'));
 
 // Uncomment the following if you want to serve up static assets.
 // (You must create the public folder)
@@ -39,7 +43,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 */
 
+
 // Handlebars config
+const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({
   layoutsDir: './views/layouts',
   defaultLayout: 'main',
@@ -47,9 +53,11 @@ app.engine('handlebars', exphbs({
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/views/`);
 
+
 // Load up all of the controllers
 const controllers = require('./controllers');
 app.use(controllers)
+
 
 // First, make sure the Database tables and models are in sync
 // then, start up the server and start listening.
