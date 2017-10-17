64ff38d01c47715069bf0f231424631518840344
Lin <linochoa11690@gmail.com>
2017-10-17 11:00:27 -0400

    added login view and controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const users = require('./users')
+const users = require('./users');
+const login = require('./login');
 //All routes go here
 
 router.use('/', require('./home'));
 router.use('/users', users);
+router.use('/login', login);
 
 module.exports = router;
