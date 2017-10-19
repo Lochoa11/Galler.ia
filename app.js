e3d01c08df53bc3118165dc096afad8ccd028c42
Andres Gajardo <andresegajardo@gmail.com>
2017-10-19 01:12:43 -0400

    Added passport use() and initialize () as per auth-example

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const expressSession = require('express-session');
 // const passport = require('passport');
 const passport = require('./middlewares/authentication');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
 const keys = require('./config/keys');
 const models = require('./models');
 
@@ -17,6 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable sessions & passport
 app.use(expressSession(({ secret: 'keyboard cat', resave: false, saveUninitialized: true })));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // passport.use(
 //   new GoogleStrategy({
@@ -46,6 +47,7 @@ app.use(express.static('./public'));
 
 // Handlebars config
 const exphbs = require('express-handlebars');
+
 app.engine('handlebars', exphbs({
   layoutsDir: './views/layouts',
   defaultLayout: 'main',
@@ -56,14 +58,15 @@ app.set('views', `${__dirname}/views/`);
 
 // Load up all of the controllers
 const controllers = require('./controllers');
-app.use(controllers)
+
+app.use(controllers);
 
 
 // First, make sure the Database tables and models are in sync
 // then, start up the server and start listening.
-models.sequelize.sync({force: false})
+models.sequelize.sync({ force: false })
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`Server is up and running on port: ${PORT}`)
+      console.log(`Server is up and running on port: ${PORT}`);
     });
-});
+  });
