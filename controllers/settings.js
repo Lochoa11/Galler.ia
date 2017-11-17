823b45a6977b85061780e6bfea93099f82408c4d
Lin <linochoa11690@gmail.com>
2017-11-17 01:02:42 -0500

    change name and change email works not change password

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,42 +1,89 @@
 const router = require('express').Router();
 const models = require('../models');
 const Redirect = require('../middlewares/redirect');
-
+const passport = require('../middlewares/authentication');
 
 router.get('/', Redirect.ifNotLoggedIn(), (req, res) => {
   res.render('settings');
 });
 
-router.put('/', (req, res) => {
-	// console.log(req.body.newFirst);
-	// models.Users.update()
-	// console.log(req.user.id);
-	// models.Users.update(
-	// 	{
-	// 		firstName:req.body.newFirst
-	// 	}, {
-	// 		where:{
-	// 			email = req.user.email
-	// 		}
-	// 	}
-	// )
-	// models.Users.find({where:{id = req.Users.id}})
-	// .on('success', function (user) {
-	// 	if(user){
-	// 		console.log(user)
-	// 		user.updateAttributes({
-	// 			firstName: req.body.newFirst,
-	// 		})
-	// 	}
-	// })
+router.post('/nameChange', (req, res) => {
+	models.Users.update(
+		{
+			firstName:req.body.newFirst.substr(0,1).toUpperCase()+req.body.newFirst.substr(1),
+			lastName: req.body.newLast.substr(0,1).toUpperCase()+req.body.newLast.substr(1),
+		}, {
+			where:{
+				id: req.user.id,
+			},
+			returning: true,
+			plain: true,		
+		}
+	).then((result) => {				
+		res.render('settings', {
+			nameChange:true,
+			newFirstName: result[1]["dataValues"]["firstName"],
+			newLastName: result[1]["dataValues"]["lastName"],
+		})
+		// console.log(result);	
+	});
 });
 
-// router.post('/settings', (req, res) => {
-	
-// });
 
-// router.post('/settings', (req, res) => {
-	
+
+router.post('/emailChange', (req, res) => {
+	// console.log("in post")
+	models.Users.update(
+		{
+			email:req.body.email,
+		},
+		{
+			where: {
+				id: req.user.id,
+			},
+			returning:true,
+			plain:true,
+		}
+	).then((result) => {				
+		res.render('settings', {
+			emailChange:true,
+			newEmail:result[1]["dataValues"]["email"],
+		})
+		// console.log(result);
+		
+	});
+});
+
+router.post('/passwordChange', (req, res) => {
+	// console.log(user.password);
+	// oldPassword = models.Users.findOne({
+	// 	where:{
+	// 		'id':req.user.id,
+	// 	}
+	// });
+
+	// if(req.body.oldPassword)
+	// console.log('hello ' + oldPassword);
+	// if(passport.passwordsMatch(req.user.newPassword,))
+
+	// console.log(
+	// 	passport.authenticate('local', {
+	// 		successRedirect: '/profile',
+	// 		failureRedirect: '/login',
+	// 	})(req, res);
+	// );
+
+	  // passport.authenticate('local', {
+	  //   successRedirect: '/settings',
+	  //   failureRedirect: '/settings',
+	  // })(req, res);	
+});
+
+// router.post('/', Redirect.ifLoggedIn(), (req, res) => {
+//   passport.authenticate('local', {
+//     successRedirect: '/profile',
+//     failureRedirect: '/login',
+//   })(req, res);
 // });
 
 // router.get('/logout', (req, res, next) => {
