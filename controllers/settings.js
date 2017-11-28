4389ec1145972a4b4b12798cbb0057d12ace21f4
Lin <linochoa11690@gmail.com>
2017-11-28 11:45:38 -0500

    change passsword working

--- controllers/settings.js
+++ controllers/settings.js
@@ -26,14 +26,10 @@ router.post('/nameChange', (req, res) => {
 			newFirstName: result[1]["dataValues"]["firstName"],
 			newLastName: result[1]["dataValues"]["lastName"],
 		})
-		// console.log(result);	
 	});
 });
 
-
-
 router.post('/emailChange', (req, res) => {
-	// console.log("in post")
 	models.Users.update(
 		{
 			email:req.body.email,
@@ -49,89 +45,39 @@ router.post('/emailChange', (req, res) => {
 		res.render('settings', {
 			emailChange:true,
 			newEmail:result[1]["dataValues"]["email"],
-		})
-		// console.log(result);
-		
+		})		
 	});
 });
 
 router.post('/passwordChange', (req, res) => {
-	console.log(req.user.password);
+	console.log("hash1 " + req.user.password);
 	if(passport.passwordsMatch(req.body.oldPassword, req.user.password)){
-		
+		console.log(req.body.oldPassword);
+		let newHash = bcrypt.hashSync(req.body.newPassword);
+		console.log("hash2 " + newHash);
+		models.Users.update(
+			{
+				password: newHash,
+			},
+			{
+				where:{
+					id: req.user.id,
+				},
+				returning: true,
+				plain:true,
+			}
+		).then(result => {
+			res.render('settings', {
+				passwordChanged: true,
+				keepOpen: true,
+			});
+		});
 	}else{
 		res.render('settings', {
 			cannotChangePassword: true,	
 			keepOpen: true,		
 		})
-	}
-
-	// let storedPassword;
-	// models.Users.findOne({
-	// 	where:{
-	// 		id:req.user.id,
-	// 	}
-	// }).then((result) =>{
-
-	// 	storedPassword = result["dataValues"]["password"];
-	// 	let oldPasswordHashed = bcrypt.hashSync(req.body.oldPassword, null);
-	// 	if(storedPassword != oldPasswordHashed){
-	// 		return;
-	// 	}
-	// });
-	
-
-	// // bcrypt.hash(req.body.oldPassword, null, null, null);
-	// console.log(oldPassword);
-
- // Users.beforeCreate(user =>
- //    new sequelize.Promise((resolve) => {
- //      bcrypt.hash(user.password, null, null, (err, hashedPassword) => {
- //        resolve(hashedPassword);
- //      });
- //    }).then((hashedPassword) => {
- //      user.password = hashedPassword;
- //    }));
-
-	// console.log(storedPassword);
-	// console.log(storedPassword);
-	// console.log(user.password);
-	// oldPassword = models.Users.findOne({
-	// 	where:{
-	// 		'id':req.user.id,
-	// 	}
-	// });
-
-	// if(req.body.oldPassword)
-	// console.log('hello ' + oldPassword);
-	// if(passport.passwordsMatch(req.user.newPassword,))
-
-	// console.log(
-	// 	passport.authenticate('local', {
-	// 		successRedirect: '/profile',
-	// 		failureRedirect: '/login',
-	// 	})(req, res);
-	// );
-
-	  // passport.authenticate('local', {
-	  //   successRedirect: '/settings',
-	  //   failureRedirect: '/settings',
-	  // })(req, res);	
+	}	
 });
-
-// router.post('/', Redirect.ifLoggedIn(), (req, res) => {
-//   passport.authenticate('local', {
-//     successRedirect: '/profile',
-//     failureRedirect: '/login',
-//   })(req, res);
-// });
-
-// router.get('/logout', (req, res, next) => {
-//   req.session.destroy((err) => {
-//     if (err) return next(err);
-//     req.logout();
-//     res.redirect('/');
-//   });
-// });
 
 module.exports = router;
--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,13 @@
     	pager: true,
     	slideWidth: 600
     });
-    // $()
-    // $('.status-new-pass')
+    $('#newPassword, #newPasswordRetyped').on('keyup', function(){
+    	if ($('#newPassword').val() == $('#newPasswordRetyped').val()) {
+	        $('#message').html('Matching').css('color', 'green');
+    	} 
+    	else{
+	        $('#message').html('Not Matching').css('color', 'red');
+    	} 
+    });
 
 })(jQuery); // End of use strict
