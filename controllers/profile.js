48aea93750fbf58e548eb4ab2bee07aedef03ff3
Lin <linochoa11690@gmail.com>
2017-11-28 13:19:58 -0500

    added user first and last to profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -44,6 +44,16 @@ router.get('/', Redirect.ifNotLoggedIn(), (req, res) => {
 });
 
 
+router.get('/edit', Redirect.ifNotLoggedIn(), (req, res) => {
+  res.render('profile', { randomPerson: req.user })
+});
+
+router.get('/settings', Redirect.ifNotLoggedIn(), (req, res) => {
+  	res.render('settings');
+  // res.sendStatus(200);
+});
+// >>>>>>> feature-editAccount
+
 
 router.get('/upload', (req, res) => {
   res.render('profile/upload');
