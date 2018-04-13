1eb57896c98fb82f303cd28323ec552731f605dc
Lin <linochoa11690@gmail.com>
2018-04-13 19:18:22 -0400

    follow feature working

--- controllers/feed.js
+++ controllers/feed.js
@@ -19,38 +19,20 @@ router.get('/', (req, res) => {
 				// res.render('feed', user.dataValues);
 				// console.log(user.dataValues.firstName);
 				const tempUserObject = [];
-				tempUserObject.push({name:user.dataValues.firstName +" "+ user.dataValues.lastName});
-				all.push(tempUserObject);
+				tempUserObject.push({name:user.dataValues.firstName +" "+ user.dataValues.lastName});				
 				// console.log(all);
 				models.Photos.findAll({
 					where:{
 						UserId:user.id,
 					},
 				}).then((photos)=>{
-					console.log(photos);
-					
-					// photos.forEach((photo) =>{
-					// 	tempUserObject.push(photo);
-					// });
-				});
-				// all.push(tempUserObject);
-				// console.log(user.dataValues.firstName);
-				// res.render('feed', user.dataValues);
+					photos.forEach((photo) =>{						
+						tempUserObject.push(photo.dataValues.image_url);
+					});
+					all.push(tempUserObject);
+				});				
 			})
-
 		});
-		// console.log(all);
-		// res.render('feed', all);
-		// console.log(Subscriptions.dataValues.user);
-		// const context = {
-		// 	Subscriptions,
-		// }
-		// models.Photos.findAll({
-		// 	where:{
-		// 		UserId: Subscriptions.user,
-		// 	},
-		// })
-		// res.render('feed', context);
 	});
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -22,9 +22,17 @@ router.get('/:id', (req, res) => {
 				UserId: user.id,
 			}
 		}).then((photos) =>{
-			res.render('users/single', {
-				user, 
-				photos,
+			models.Subscriptions.findOne({
+				where:{
+					UserId: req.user.id,
+					user: req.params.id,
+				}
+			}).then((subscription) => {
+				res.render('users/single', {
+					user, 
+					photos,
+					subscription,
+				});
 			});
 		});
 	}).catch(() => {		
@@ -35,6 +43,62 @@ router.post('/follow', (req, res) => {
 	models.Subscriptions.create({
 		user:req.body.UserId,//this is from the account user wants to follow
 		UserId: req.user.id,//this is the user performing the "following"/"Subscribing"
+	}).then(() => {
+		models.Users.findOne({
+			where:{
+				id:req.body.UserId,
+			},
+		}).then((user) =>{
+			models.Photos.findAll({
+				order:['createdAt'],
+				where:{
+					UserId: user.id,
+				},
+			}).then((photos) =>{
+				models.Subscriptions.findOne({
+					where:{
+						UserId: req.user.id,
+						user: req.body.UserId,
+					}
+				}).then((subscription) => {
+					res.render('users/single', {
+						user, 
+						photos,
+						subscription,
+					});
+				});
+			});
+		}).catch(() => {		
+		});
+	});
+});
+
+router.post('/unfollow', (req, res) => {
+
+	models.Subscriptions.destroy({
+		where:{
+			user: req.body.UserId,
+			UserId: req.user.id,
+		}
+	}).then(() => {
+		models.Users.findOne({
+			where:{
+				id:req.body.UserId,
+			},
+		}).then((user) =>{
+			models.Photos.findAll({
+				order:['createdAt'],
+				where:{
+					UserId: user.id,
+				},
+			}).then((photos) =>{
+				res.render('users/single', {
+					user, 
+					photos,
+				});
+			});
+		}).catch(() => {		
+		});
 	});
 });
 
